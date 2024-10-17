b6fdb08ac7beb30bc1da8f0374553199254ecb25
Baghurst <23bbaghurst.acc@lwetb.ie>
2024-10-17 12:01:05 +0100

    Update PythonDataTypesEx1.py

--- PythonDataTypesEx1.py
+++ PythonDataTypesEx1.py
@@ -1,20 +1,20 @@
 #1
 
 print("Hello user")
-avgtemp = float(input("Tell me the average temperature of the past week: "))
+monday_temp = float(input("Tell me the temperature of this last monday: "))
+tuesday_temp = float(input("Tell me the temperature of this last tuesday: "))
+wednesday_temp = float(input("Tell me the temperature of this last wednesday: "))
+thursday_temp = float(input("Tell me the temperature of this last thursday: "))
+friday_temp = float(input("Tell me the temperature of this last friday: "))
+saturday_temp = float(input("Tell me the temperature of this last saturday: "))
+sunday_temp = float(input("Tell me the temperature of this last sunday: "))
+avgtemp = monday_temp + tuesday_temp + wednesday_temp + thursday_temp + friday_temp + saturday_temp + sunday_temp / 7
+print(f"The average temperature of this week is {avgtemp}")
 
+#2
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+PI=3.14
+x_ = float(input("Enter the value of x: "))
+y_ = float(input("Enter the value of y: "))
+z_ = float(input("Enter the value of z: "))
+print(4*x_**4 + 3*y_**3 + 9*z_ + 6 * PI)
